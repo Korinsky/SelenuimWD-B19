aa3c7bc76880321cdde4c8b41e5e561319bc306d
Roman Klochkov <72462941+Korinsky@users.noreply.github.com>
2020-10-19 18:02:47 +0300

    update 7 & 8 tasks

--- task7.py
+++ task7.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import os
 
 @pytest.fixture
 def driver(request):
@@ -14,9 +13,7 @@ def driver(request):
 
 
 def test_example(driver):
-    file = open("report.txt", "w")
     wait = WebDriverWait(driver, 10)
-    menu_dict = {'Appearence': ('Template', 'Logotype'), 'Catalog': ('Catalog', 'Product Groups', 'Option Groups', 'Manufacturers', 'Suppliers', 'Delivery Statuses', 'Sold Out Statuses', 'Quantity Units', 'CSV Import/Export'), 'Countries': [], 'Currencies': [], 'Customers': ('Customers', 'CSV Import/Export', 'Newsletter'), 'Geo Zones': [], 'Languages': ('Languages', 'Storage Encoding'), 'Modules': ('Background Jobs', 'Customer', 'Shipping', 'Payment', 'Order Total', 'Order Success', 'Order Action'), 'Orders': ('Orders', 'Order Statuses'), 'Pages': [], 'Reports': ('Monthly Sales', 'Most Sold Products', 'Most Shopping Customers'), 'Settings': ('Store Info', 'Defaults', 'General', 'Listings', 'Images', 'Checkout', 'Advanced', 'Security'), 'Slides': [], 'Tax': ('Tax Classes', 'Tax Rates'), 'Translations': ('Search Translations', 'Scan Files', 'CSV Import/Export'), 'Users': [], 'vQmods': ['vQmods']}
 
     driver.get("http://localhost/litecard/admin")
     wait.until(EC.presence_of_element_located((By.XPATH, "//input[@name='username']")))
@@ -24,31 +21,19 @@ def test_example(driver):
     driver.find_element_by_name("password").send_keys("admin")
     driver.find_element_by_name("login").click()
 
-    for top_menu_num in range(1, len(menu_dict.keys())+1):
-        top_menu = driver.find_element_by_xpath("//ul[@id='box-apps-menu']/li[%i]" % top_menu_num)
-        top_menu.click()
+    top_menu_list = driver.find_elements_by_xpath("//li[@id='app-']")
 
-        if driver.find_elements_by_xpath("//h1") == []:
-            file.write("Menu '%s' has no title\n" % top_menu_num)
+    for top_menu_num in range(1, len(top_menu_list) + 1):
+        driver.find_element_by_xpath("//li[@id='app-'][%i]" % top_menu_num).click()
 
-        top_menu = list(menu_dict.keys())[top_menu_num-1]
-        sub_menu = menu_dict.get(top_menu)
+        assert EC.presence_of_element_located((By.XPATH, "//h1")), "Menu has no title"
 
-        if len(sub_menu) == 0:
+        if not EC.presence_of_element_located((By.XPATH, "//ul[@class='docs']")):
             continue
 
-        sub_menu_list = len(menu_dict.get(top_menu))
-        for sub_menu_num in range(1, sub_menu_list+1):
-            wait.until(EC.presence_of_element_located((By.XPATH, "//ul[@class='docs']/li[%i]" % sub_menu_num)))
-            sub_menu = driver.find_element_by_xpath("//ul[@class='docs']/li[%i]" % sub_menu_num)
-            sub_menu.click()
+        sub_menu_list = driver.find_elements_by_xpath("//li/ul/li")
 
-            if driver.find_elements_by_xpath("//h1") == []:
-                file.write("Sub-menu '%s' in menu '%s' hase no title\n" % (sub_menu, top_menu))
+        for sub_menu_num in range(1, len(sub_menu_list) + 1):
+            driver.find_element_by_xpath("//li/ul/li[%i]" % sub_menu_num).click()
 
-    size = os.path.getsize("report.txt")
-    if size == 0:
-        file.write("all headers are present")
-    file.close()
-
-
+            assert EC.presence_of_element_located((By.XPATH, "//h1")), "Menu has no title"
--- task8.py
+++ task8.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import os
 
 
 @pytest.fixture
@@ -15,23 +14,13 @@ def driver(request):
 
 
 def test_example(driver):
-    file = open("report.txt", "w")
     wait = WebDriverWait(driver, 10)
     driver.get("http://localhost/litecard")
     wait.until(EC.presence_of_element_located((By.ID, "box-most-popular")))
 
-    item_list = driver.find_elements_by_css_selector(".middle>.content>.box li")
+    item_list = driver.find_elements_by_css_selector(".product")
 
     for item in item_list:
         sticker = item.find_elements_by_css_selector(".sticker")
 
-        if len(sticker) != 1:
-            name = item.find_element_by_css_selector(".name")
-            file.write("Product '%s' has no one sticker\n" % name.text)
-
-    file.close()
-    size = os.path.getsize("report.txt")
-    if size == 0:
-        file = open("report.txt", "a")
-        file.write("all products have one sticker")
-        file.close()
+        assert len(sticker) == 1, "not one sticker"

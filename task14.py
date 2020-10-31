6ba2cd33ce51e484b48d381f07cd95c22c6c50c9
Roman Klochkov <72462941+Korinsky@users.noreply.github.com>
2020-11-01 01:06:37 +0300

    add BrowserStack.png

--- task14.py
+++ task14.py
@@ -3,9 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-import os
 import time
 
 
